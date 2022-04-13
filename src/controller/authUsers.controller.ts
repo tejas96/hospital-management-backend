1ab14d0440e750cf5bbf7e7d667e9ad30911cea7
tejas96 <tejas96patil@gmail.com>
2022-04-13 13:35:43 +0530

    add otp apis

--- src/controller/authUsers.controller.ts
+++ src/controller/authUsers.controller.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
+import { sendOTP } from "../utility";
 import Admin from "../config/firebase";
+
+const otpTable = {
+  "123456789": 123456,
+} as any;
 
 export const fetchLoggedInUser = async (req: Request, res: Response) => {
   const { uid } = <{ uid: string }>req.query;
@@ -11,4 +16,26 @@ export const fetchLoggedInUser = async (req: Request, res: Response) => {
   if (!currentlyLoggedInUserData.data())
     return res.status(400).send("User not found");
   return res.status(200).send(currentlyLoggedInUserData.data());
+};
+
+export const sendOtpHandler = async (req: Request, res: Response) => {
+  const { phoneNumber } = req.params;
+  const otp = Math.floor(100000 + Math.random() * 900000);
+  const message = `Your OTP is ${otp}`;
+  otpTable[phoneNumber] = otp;
+  try {
+    sendOTP("+91" + phoneNumber, message);
+  } catch (e) {
+    return res.status(400).send("Error sending OTP");
+  }
+  return res.status(200).send("OTP sent successfully");
+};
+
+export const verifyOtpHandler = async (req: Request, res: Response) => {
+  const { phoneNumber, otp } = req.body;
+  if (otpTable[phoneNumber + ""] === Number(otp)) {
+    delete otpTable[phoneNumber];
+    return res.status(200).send("OTP verified");
+  }
+  return res.status(400).send("OTP not verified");
 };
--- src/routes/authUsers.routes.ts
+++ src/routes/authUsers.routes.ts
@@ -3,5 +3,6 @@ import { AuthUserController } from "../controller";
 const router = Router();
 
 router.get("/loggedInUser", AuthUserController.fetchLoggedInUser);
-
+router.get("/sendOtp/:phoneNumber", AuthUserController.sendOtpHandler);
+router.post("/verifyOtp", AuthUserController.verifyOtpHandler);
 export default router;
